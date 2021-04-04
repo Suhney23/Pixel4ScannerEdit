1a8b1efc99764e243fb19d0e6f4e71666b66af05
Suhney23 <sma227@njit.edu>
2021-04-04 10:21:57 -0400

    Initial commit

---
+++ app/src/main/java/ru/semper_viventem/pixel4scanner/GalleryActivity.java
@@ -0,0 +1,14 @@
+package ru.semper_viventem.pixel4scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+    }
+}
